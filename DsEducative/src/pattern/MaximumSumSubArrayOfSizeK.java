a5e2e9b4174737d3d6cb90c943dd7fa306ea4650
vishvishnu12 <anandvas20@gmail.com>
2020-01-04 22:33:03 +0530

    Project folder setup created

---
+++ DsEducative/src/pattern/MaximumSumSubArrayOfSizeK.java
@@ -0,0 +1,14 @@
+package pattern;
+
+/**
+ * Problem Statement # Given an array of positive numbers and a positive number
+ * ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
+ * 
+ * EX1: Input: [2, 1, 5, 1, 3, 2], k=3 Output: 9 Explanation: Subarray with
+ * maximum sum is [5, 1, 3].
+ *
+ * 
+ */
+public class MaximumSumSubArrayOfSizeK {
+
+}
